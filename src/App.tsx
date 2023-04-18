2f8d089543eb05ca7f585d4e80e5dac6074984e8
crow-fox <surakamiya198@gmail.com>
2023-04-18 13:05:02 +0900

    react-router導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { type FC } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App: FC = () => {
   return (
     <div>
       <h1>Vite + React</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
